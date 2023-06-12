3fdb89644fdcd57b546014c970c6c8a32333e375
MatMosss <matisse.mossard@gmail.com>
2023-06-12 18:29:55 +0200

    latitude modif

--- Matisse.r
+++ Matisse.r
@@ -1,8 +1,16 @@
-Data <- read.csv2(file = "stat_MossMoss.csv")
+data <- read.csv2(file = "stat_MossMoss.csv")
+infos <- read.csv2(file = "cities.csv")
+data[Data == "NULL"] <- NA
 
-Data[Data == "NULL"] <- NA
 
-# print(which(is.na(Data), arr.ind = TRUE))
-print(sapply(Data,class))
-Data$latitude <- as.numeric(Data$latitude)
-Data$longitude <- as.numeric(Data$longitude)
+
+data$latitude <- as.numeric(data$latitude)
+data$longitude <- as.numeric(data$longitude)
+data$an_nais <- as.numeric(data$an_nais)
+data$age <- as.numeric(data$age)
+data$id_code_insee <- as.numeric(data$id_code_insee)
+# print(sapply(data,class))
+# print(class(data$id_code_insee))
+
+
+print(data[data$latitude == 2009, ])
