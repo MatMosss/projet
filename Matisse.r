3067113eef34b171243277d78f3536f4256aad80
MatMosss <matisse.mossard@gmail.com>
2023-06-12 15:41:31 +0200

    modif

--- Matisse.r
+++ Matisse.r
@@ -1,7 +1,7 @@
 Data <- read.csv2(file = "stat_MossMoss.csv")
 # print(Data$date)
-#  print(is.na(Data))
 # print(which(is.na(Data),arr.ind=TRUE))
 # print(summary(Data))
 # read.csv(text=bt, na.string = "abc")
 Data[Data == "NULL"] <- NA
+print(is.na(Data))
