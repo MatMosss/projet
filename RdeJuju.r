2b59c306ff4bee9ae31099bff2c1c19cd5a08e71
julienYncrea <julien.minvielle@isen-ouest.yncrea.fr>
2023-06-12 16:20:15 +0200

    add

--- RdeJuju.r
+++ RdeJuju.r
@@ -1,1 +1,16 @@
-import "stat_acc_V3.csv"
+
+BDD <- read.csv2(file = "stat_juju.csv")
+
+dim(BDD)
+names(BDD)
+summary(BDD)
+head(BDD)
+nombre <- dim(BDD)[1]
+print(nombre)
+
+counts <- table(BDD$descr_athmo)
+
+print(counts)
+
+
+print("Fin du code")
