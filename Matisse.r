311662eb2adb1d716bf256cd917af9c3fda0472b
MatMosss <matisse.mossard@gmail.com>
2023-06-12 15:39:44 +0200

    modif

--- Matisse.r
+++ Matisse.r
@@ -1,1 +1,7 @@
-lhgqiuhgkljsljkr
+Data <- read.csv2(file = "stat_MossMoss.csv")
+# print(Data$date)
+#  print(is.na(Data))
+# print(which(is.na(Data),arr.ind=TRUE))
+# print(summary(Data))
+# read.csv(text=bt, na.string = "abc")
+Data[Data == "NULL"] <- NA
