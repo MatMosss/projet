7aa5f9c7c933ccb934cdf01f1d0c9519314f1c84
MatMosss <matisse.mossard@gmail.com>
2023-06-12 16:18:39 +0200

    change type values

--- Matisse.r
+++ Matisse.r
@@ -1,8 +1,8 @@
 Data <- read.csv2(file = "stat_MossMoss.csv")
-# print(Data$date)
-# print(which(is.na(Data),arr.ind=TRUE))
-# print(summary(Data))
-# read.csv(text=bt, na.string = "abc")
+
 Data[Data == "NULL"] <- NA
-# print(is.na(Data))
-print(which(is.na(Data),arr.ind=TRUE))
+
+# print(which(is.na(Data), arr.ind = TRUE))
+print(sapply(Data,class))
+Data$latitude <- as.numeric(Data$latitude)
+Data$longitude <- as.numeric(Data$longitude)
