e65e0595ddf6e5aeea5479f81300e4c55ddb0229
MatMosss <matisse.mossard@gmail.com>
2023-06-12 22:56:22 +0200

    traitement donées

--- Matisse.r
+++ Matisse.r
@@ -1,6 +1,6 @@
 data <- read.csv2(file = "stat_MossMoss.csv")
-infos <- read.csv2(file = "cities.csv")
-data[Data == "NULL"] <- NA
+infos <- read.csv2(file = "cities.csv", sep = ",")
+data[data == "NULL"] <- NA
 
 
 
@@ -12,5 +12,26 @@ data$id_code_insee <- as.numeric(data$id_code_insee)
 # print(sapply(data,class))
 # print(class(data$id_code_insee))
 
+infos$insee_code <- as.numeric(infos$insee_code)
+infos <- subset(infos, !duplicated(insee_code))
 
-print(data[data$latitude == 2009, ])
+# compteur <- 1
+# for (lat in data$latitude) {
+#   if (lat == 2009 || lat == 0) {
+#     data[compteur, "longitude"] <- subset(infos, insee_code == data[compteur, "id_code_insee"], longitude)
+#     data[compteur, "latitude"] <- subset(infos, insee_code == data[compteur, "id_code_insee"], latitude)
+#   }
+#   compteur <- compteur+1
+# }
+# compteur <- 1
+# for (place in data$place) {
+#   if (is.na(place)) {
+#     data[compteur, "place"] <- 0
+#   }
+#   compteur <- compteur+1
+# }
+
+print(class(data[908, "id_code_insee"]))
+# print(data[data$latitude == 2009, ])
+# print(which(is.na(data),arr.ind=TRUE))
+# print(subset(infos, insee_code == 97414,longitude))
