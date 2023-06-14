ea6eb9ac62e3323d7ec4a034348ffe15fc05df06
julienYncrea <julien.minvielle@isen-ouest.yncrea.fr>
2023-06-14 12:00:42 +0200

    add

--- RdeJuju.r
+++ RdeJuju.r
@@ -1,4 +1,5 @@
-BDD <- read.csv2(file = "stat_Ninho.csv")
+BDD <- read.csv2(file = "stat_juju.csv")
+library(ggplot2)
 BDD[BDD == "NULL"] <- " NA"
 dim(BDD)
 names(BDD)
@@ -23,65 +24,151 @@ fonction_visu <- function(colonne){
                 compteur <- compteur + 1
         }
     }
-
+    
     list_nb <- c(list_nb, compteur)
     list_type <- c(list_type, elt)
     }
-
     return(list(list_nb = list_nb, list_type = list_type))
 }
+affiche_pie_athmo <- function(){
+    valeur <- fonction_visu(BDD$descr_athmo)
 
-par(mfrow = c(1, 5)) #créer un tableau pour mettre les pie à coté
-valeur <- fonction_visu(BDD$descr_athmo)
-
-valeur1 <- valeur$list_nb
-valeur2 <- valeur$list_type
-pie(valeur1, labels = valeur2, main = "nb d'accident en fct des cdt athmo")
-legend("topleft", legend = "sachant qu'en moyenne il pleut 15,2% du temps 
+    valeur1 <- valeur$list_nb
+    valeur2 <- valeur$list_type
+    dfathmo <- data.frame(valeur1 = valeur$list_nb, valeur2 = valeur$list_type)
+    figure <- ggplot(data = dfathmo, aes(x = "", y = valeur1, fill = valeur2)) +
+        geom_bar(stat = "identity", width = 1) +
+        coord_polar(theta = "y") +
+        labs(title = "Répartition des accidents en fonction 
+        des conditions atmosphériques") +
+        theme_void()
+    print("sachant qu'en moyenne il pleut 15,2% du temps 
 (24*365/576 où 576 est le nombre d'heure de pluie légère en moyenne 
 en france en 2019) or 7296/73563*100 = 10% d'accident lié à la pluie legère
 on ne peut en déduire que les accidents dépendent 
-de conditions atmosphériques "
-, bty = "n")
-#bty supp le cadre autour de la légende sinon bug
-surface <- fonction_visu(BDD$descr_etat_surf)
-surf1 <- surface$list_nb
-surf2 <- surface$list_type
+de conditions atmosphériques ")
+    print(figure)
+}
+affiche_pie_surface <- function(){
+    surface <- fonction_visu(BDD$descr_etat_surf)
+    surf1 <- surface$list_nb
+    surf2 <- surface$list_type
+    dfathmo <- data.frame(surf1 = surface$list_nb, surf2 = surface$list_type)
 
-pie(surf1, labels = surf2, main = "nb d'accident en fct de la surface")
- legend("topleft", legend = "Ici on peut voir que sur sol mouillé, 
- 12250/73643*100 = 16,6% or il pleut en moyenne 15% du temps 
- et la pluie peut mettre du temps à sécher, 
- on ne peut pas conclure que les accidents sont lié à la surface"
- , bty = "n")
-ville <- fonction_visu(BDD$ville)
-ville1 <- ville$list_nb[ville$list_nb > 500]
-ville2 <- ville$list_type[ville$list_nb > 500]
-print(ville2)
-print(ville1)
-barplot(ville1, names.arg = ville2, main = "nb d'accident en fct de la ville",
-xlab = "ville", ylab = "nb accident", las=2)
-#las fait pivoter les noms du bas
-date <- BDD$date
-listV <- c(0, 0, 0, 0, 0, 0)
-listV <- as.numeric(listV)
-compteur =0
-for(elt in date){
-    compteur = compteur +1
-    heure <- substr(elt, start=12, stop=20)
-    splitH <- strsplit(heure, split= ":")
-    for(i in seq(4, 28, by = 4)){
-    if( i-4 < as.numeric(splitH[[1]][1]) && as.numeric(splitH[[1]][1]) < i){
-        nb = i/4
-        listV[nb] = listV[nb]+1
+    figure1 <- ggplot(data = dfathmo, aes(x = "", y = surf1, fill = surf2)) +
+        geom_bar(stat = "identity", width = 1) +
+        coord_polar(theta = "y") +
+        labs(title = "Répartition des accidents en fonction 
+        de la surface") +
+        theme_void()
+    print("Ici on peut voir que sur sol mouillé, 
+    12250/73643*100 = 16,6% or il pleut en moyenne 15% du temps 
+    et la pluie peut mettre du temps à sécher, 
+    on ne peut pas conclure que les accidents sont lié à la surface")
+    print(figure1)
+    }
+affiche_barre_ville <- function(){
+    ville <- fonction_visu(BDD$ville)
+    ville1 <- ville$list_nb[ville$list_nb > 500]
+    ville2 <- ville$list_type[ville$list_nb > 500]
+    dfville <- data.frame(ville1, ville2)
+    figure <- ggplot(dfville, aes(x = ville2, y = ville1)) +
+    geom_bar(stat = "identity") +
+    labs(title = "Nombre d'accidents par ville", 
+    x = "Ville", y = "Nombre d'accidents") +
+    theme_void()
+    print(figure)
+    }
+
+affiche_pie_heure() <- function(){
+    date <- BDD$date
+    listV <- c(0, 0, 0, 0, 0, 0)
+    listV <- as.numeric(listV)
+    compteur =0
+    for(elt in date){
+        compteur = compteur +1
+        heure <- substr(elt, start=12, stop=20)
+        splitH <- strsplit(heure, split= ":")
+        for(i in seq(4, 28, by = 4)){
+            if( i-4 < as.numeric(splitH[[1]][1]) && as.numeric(splitH[[1]][1]) < i){
+            nb = i/4
+            listV[nb] = listV[nb]+1
         }
     }
+    }
+    tranche_heure = c("0-4h", "4-8h", "8-12h", "12-16h", "16-20h", "20-24h")
+    dfheure <- data.frame(listV, tranche_heure)
+    figure2 <- ggplot(data = dfheure, aes(x = "", y = listV, fill = tranche_heure)) +
+        geom_bar(stat = "identity", width = 1) +
+        coord_polar(theta = "y") +
+        labs(title = "Répartition des accidents en fonction 
+        des conditions atmosphériques") +
+        theme_void()
+    print(figure2)
 }
-tranche_heure = c("0-4h", "4-8h", "8-12h", "12-16h", "16-20h", "20-24h")
-pie(listV, labels = tranche_heure, main = "nb d'accident en fct de l'heure")
-gravite <- fonction_visu(BDD$descr_grav)
-grav1 <- gravite$list_nb
-grav2 <- gravite$list_type
+affiche_pie_gravite <- function(){
+    gravite <- fonction_visu(BDD$descr_grav)
+    grav1 <- gravite$list_nb
+    grav2 <- gravite$list_type
+    dfgrav <- data.frame(grav1, grav2)
+    figure1 <- ggplot(data = dfgrav, aes(x = "", y = grav1, fill = grav2)) +
+        geom_bar(stat = "identity", width = 1) +
+        coord_polar(theta = "y") +
+        labs(title = "Répartition des accidents en fonction 
+        de la surface") +
+        theme_void()
+    print(figure1)
+}
+ecriture_csv <- function(){
+    listeVide = c()
+    for(elt in BDD$date){
+        mois <- substr(elt, start=6, stop=7)
+        listeVide <- append(listeVide, mois)
+    }
+    BDD$mois <- listeVide
 
-pie(grav1, labels = grav2, main = "nb d'accident en fct de la gravité")
+    BDD$date1 <- as.Date(BDD$date, format = "%Y-%m-%d %H:%M:%S")
+    BDD$semaine <- format(BDD$date1, "%U")
+    write.csv(BDD, "stat_juju3.csv", row.names = FALSE)
+}
+BDD2 <- read.csv2(file = "stat_juju3.csv", sep = ",")
+
+affiche_serie_mois <- function(){
+    valeur1 <- fonction_visu(BDD2$mois)
+    nbaccident <- valeur1$list_nb
+    numois <- valeur1$list_type
+
+    dictionnaire_mois <- c("Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Sept", "Octo", "Nov", "Déc")
+    dfmois = data.frame(nbaccident, numois)
+    figure2 <- ggplot(data = dfmois, aes(x = "", y = nbaccident, fill = numois)) +
+        geom_bar(stat = "identity", width = 1) +
+        coord_polar(theta = "y") +
+        labs(title = "Répartition des accidents en fonction 
+        des conditions atmosphériques") +
+        theme_void()
+    print(figure2)
+}
+# valeur2 <- fonction_visu(BDD2$semaine)
+# nbaccidentparsemaine <- valeur2$list_nb
+# numsemaine <- valeur2$list_type
+# numsemaine <- as.numeric(numsemaine)
+# nbaccidentparsemaine <- nbaccidentparsemaine[order(numsemaine)]
+# numsemaine <- sort(numsemaine)
+# print(nbaccidentparsemaine)
+# print(numsemaine)
+# plot(numsemaine, nbaccidentparsemaine, type = "b", col = "red",
+#      xlab = "numsemaine", ylab = "nbaccident",
+#      main = "série chronologique de nb d'accident par semaine")
+# legend("bottom", legend = "On remarque que la distribution générale des 
+# accidents dans le temps ne suit pas un modèle linéaire.
+# Cependant, et justement car la variabilité des accidents 
+# est grande entre deux intervalles de temps 
+# (cela se remarque d'autant plus dans le cas
+#  des semaines, où les fluctuations
+# de signe de la dérivée sont très nombreuses) 
+# on peut estimer qu'un modèle linéaire
+#  conviendrait mieux à la
+#  distribution mensuelle des accidents.  "
+# , bty = "n")
+# print(table(BDD$semaine))
 print("fin de code")
---
+++ main.r
@@ -0,0 +1,4 @@
+source("Rdejuju.r")
+affiche_pie_athmo()
+#affiche_pie_surface()
+#affiche_barre_ville()
