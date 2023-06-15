cebfbef820bdc2993dfa4f023cf4f16dea5ca541
redalamr <medredaelam@gmail.com>
2023-06-15 20:54:43 +0200

    histo

--- main.r
+++ main.r
@@ -2,12 +2,8 @@ source("Matisse_traitement_donnee.r")
 source("Matisse_sql.r")
 source("RdeJuju.r")
 source("histogrammes.r")
-<<<<<<< HEAD
 source("population100.R")
 source("regression.R")
-=======
-source("tests d’indépendance du chi2.r")
->>>>>>> ee6dfbdc8b94720db280874708296350e0305141
 
 # library(sqldf)
 # library(dplyr)
@@ -26,7 +22,6 @@ require(FactoMineR)
 
 main <- function(){
     # traitement_donnees()
-<<<<<<< HEAD
     affiche_pie_athmo()
     affiche_pie_surface()
     #affiche_barre_ville()
@@ -39,15 +34,11 @@ main <- function(){
     download_graph_departement()
     download_graph_regions()
     Histogramme()
-<<<<<<< HEAD
     regressions()
     ACP()
 }
-=======
     tests_tableaux_indépendance ()
 }
->>>>>>> d5fbf63c575aecde445883775110ac1c09cb4da1
-=======
     # affiche_pie_athmo()
     # affiche_pie_surface()
     # #affiche_barre_ville()
@@ -63,4 +54,3 @@ main <- function(){
     tests_tableaux_independance ()
 }
 
->>>>>>> ee6dfbdc8b94720db280874708296350e0305141
