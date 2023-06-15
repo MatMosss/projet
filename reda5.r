9549dd52aed3595544ebf992d4403febb96ec1fd
redalamr <medredaelam@gmail.com>
2023-06-15 16:26:11 +0200

    redaaa

--- reda5.r
+++ reda5.r
@@ -1,17 +1,25 @@
 install.packages(c("tidyverse","questionr"))
 library(tidyverse)
 library(questionr)
-library(dplyr)
-library(tidyverse)
-library(questionr)
 
 df <- read_delim("redol.csv", delim = ";")
 
+# Appliquer la fonction freq() à descr_type_col
+freq_ville_new <- freq(df$descr_type_col, valid = FALSE, total = TRUE)
+print(freq_ville_new, max = nrow(freq_ville_new))
 
-freq_ville_new <- freq(df$descr_type_col, valid=FALSE,total=TRUE )
+# Sélection automatique des colonnes de type caractère ou facteur
+colonnes <- df %>% select_if(function(col) {
+  is.character(col) || is.factor(col)
+})
 
-print(freq_ville_new,max = nrow(freq_ville_new))
-df %>% select(descr_athmo) %>% table() 
-df %>% select(descr_type_col) %>% table() 
-df %>% select(descr_lum) %>% table() 
-df %>% select(*) %>% table() 
+# Appliquer la fonction table() à chaque colonne sélectionnée
+resultats <- lapply(colonnes, table)
+
+# Afficher les résultats
+names(resultats) <- colnames(colonnes)
+print(resultats)
+
+# Calcul des tableaux de contingence
+df_contingency <- do.call(rbind, lapply(df, table))
+print(df_contingency)
