47ddb242f437401a7f08b03d1e1af8b5e021b733
julienYncrea <julien.minvielle@isen-ouest.yncrea.fr>
2023-06-15 17:45:55 +0200

    ajouter

--- RdeJuju.r
+++ RdeJuju.r
@@ -44,14 +44,15 @@ affiche_pie_athmo <- function(){
     figure <- ggplot(data = dfathmo, aes(x = "", y = valeur1, fill = valeur2)) +
         geom_bar(stat = "identity", width = 1) +
         coord_polar(theta = "y") +
-        labs(title = "Répartition des accidents en fonction 
-        des conditions atmosphériques") +
-        theme_void()
+        labs(title = "Répartition des accidents en fonction des conditions atmosphériques",fill = "donnée athmosphérique",
+    x = "", y="nb d'accident") +
+        theme(plot.title = element_text(face = "bold", hjust=0.5))
     print("sachant qu'en moyenne il pleut 15,2% du temps 
 (24*365/576 où 576 est le nombre d'heure de pluie légère en moyenne 
 en france en 2019) or 7296/73563*100 = 10% d'accident lié à la pluie legère
 on ne peut en déduire que les accidents dépendent 
 de conditions atmosphériques ")
+    ggsave("figure/figureathmo.png", figure, dpi =300)
     print(figure)
 }
 affiche_pie_surface <- function(){
@@ -63,14 +64,15 @@ affiche_pie_surface <- function(){
     figure1 <- ggplot(data = dfathmo, aes(x = "", y = surf1, fill = surf2)) +
         geom_bar(stat = "identity", width = 1) +
         coord_polar(theta = "y") +
-        labs(title = "Répartition des accidents en fonction 
-        de la surface") +
-        theme_void()
+        labs(title = "Répartition des accidents en fonction de la surface",fill = "donnée surfacique",
+    x = "", y="nb d'accident") +
+        theme(plot.title = element_text(face = "bold", hjust=0.5))
     print("Ici on peut voir que sur sol mouillé, 
     12250/73643*100 = 16,6% or il pleut en moyenne 15% du temps 
     et la pluie peut mettre du temps à sécher, 
     on ne peut pas conclure que les accidents sont lié à la surface")
     print(figure1)
+    ggsave("figure/figuresurface.png", figure1, dpi =300)
     }
 affiche_barre_ville <- function(){
     ville <- fonction_visu(BDD$ville, 0)
@@ -81,11 +83,12 @@ affiche_barre_ville <- function(){
     geom_bar(stat = "identity") +
     labs(title = "Nombre d'accidents par ville",
          x = "Ville", y = "Nombre d'accidents") +
-    theme_minimal() +
+    theme(plot.title = element_text(face = "bold", hjust = 0.5)) +
     theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
-    scale_fill_gradient(low = "blue", high = "red")
-    #scale_fill_viridis_d()
+    scale_fill_discrete()
     print(figure)
+    ggsave("figure/figureville.png", figure, dpi =300)
+
     }
 
 affiche_pie_heure <- function(){
@@ -105,29 +108,33 @@ affiche_pie_heure <- function(){
         }
     }
     }
-    
     tranche_heure = c("0-4h", "4-8h", "8-12h", "12-16h", "16-20h", "20-24h")
     dfheure <- data.frame(listV, tranche_heure)
-    figure2 <- ggplot(data = dfheure, aes(x = "", y = listV, fill = tranche_heure)) +
-        geom_bar(stat = "identity", width = 1) +
-        coord_polar(theta = "y") +
-        labs(title = "Répartition des accidents en fonction 
-        des conditions atmosphériques") +
-        theme_void()
+    figure2 <- ggplot(data = dfheure, aes(x = tranche_heure, y = listV, fill = tranche_heure)) +
+        geom_bar(stat = "identity") +
+    labs(title = "Nombre d'accidents par tranche horraire",
+         x = "horraire", y = "Nombre d'accidents") +
+    theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+    scale_fill_discrete()
     print(figure2)
+    ggsave("figure/figureheure.png", figure2, dpi =300)
+
 }
 affiche_pie_gravite <- function(){
     gravite <- fonction_visu(BDD$descr_grav, 0)
     grav1 <- gravite$list_nb
     grav2 <- gravite$list_type
     dfgrav <- data.frame(grav1, grav2)
-    figure1 <- ggplot(data = dfgrav, aes(x = "", y = grav1, fill = grav2)) +
+    gravite <- c("Indemne",  "Tué", "blessé hospitalisé", "blessé léger")
+    figure1 <- ggplot(data = dfgrav, aes(x = "", y = grav1, fill = gravite)) +
         geom_bar(stat = "identity", width = 1) +
         coord_polar(theta = "y") +
-        labs(title = "Répartition des accidents en fonction 
-        de la surface") +
-        theme_void()
+        labs(title = "Répartition des accidents en fonction de la gravité",fill = "donnée gravité",
+    x = "", y="nb d'accident") +
+        theme(plot.title = element_text(face = "bold", hjust=0.5))
     print(figure1)
+        ggsave("figure/figuregrav.png", figure1, dpi =300)
+
 }
 
 ecriture_csv <- function(){
@@ -144,8 +151,8 @@ ecriture_csv <- function(){
 }
 BDD2 <- read.csv2(file = "stat_juju3.csv", sep = ",")
 
-affiche_serie_mois <- function(){
-    valeur1 <- fonction_visu(BDD2$mois, 1)
+affiche_serie_mois <- function(CUMUL){
+    valeur1 <- fonction_visu(BDD2$mois, CUMUL)
     nbaccident <- valeur1$list_nb
     numois <- valeur1$list_type
 
@@ -155,16 +162,23 @@ affiche_serie_mois <- function(){
     numois <- as.numeric(numois)
     dfmois = data.frame(nbaccident, numois)
     print(dfmois)
-    figure2 <- ggplot(data = dfmois, aes(x = numois, y = nbaccident)) +
+    figure2 <- ggplot(data = dfmois, aes(x = numois, y = nbaccident, fill = nbaccident)) +
     geom_line() +
-        labs(title = "Répartition des accidents en fonction 
-             des conditions atmosphériques", x = "les mois", y = "Nombre d'accidents" ) +
-        theme_minimal()
+        labs(title = "série chronologique des accidents en fonction des mois", x = "les mois", y = "Nombre d'accidents" ) +
+        theme() + theme(panel.background = element_rect(fill = "white"))+
+        scale_x_continuous(breaks = numois, labels = dictionnaire_mois)
     print(figure2)
+    if(CUMUL== 0){
+    ggsave("figure/figureseriemois.png", figure2, dpi =300)
+    }
+    if(CUMUL== 1){
+    ggsave("figure/figureseriemoiscumul.png", figure2, dpi =300)
+    }
+
  }
  # geom_point() + geom_line() +
-affiche_serie_semaine <- function(){
-    valeur2 <- fonction_visu(BDD2$semaine, 1)  
+affiche_serie_semaine <- function(CUMUL){
+    valeur2 <- fonction_visu(BDD2$semaine, CUMUL)  
     nbaccidentparsemaine <- valeur2$list_nb
     numsemaine <- valeur2$list_type
     numsemaine <- as.numeric(numsemaine)
@@ -174,9 +188,8 @@ affiche_serie_semaine <- function(){
     dfsemaine = data.frame(nbaccidentparsemaine, numsemaine)
     figure4 <- ggplot(data = dfsemaine, aes(x = numsemaine, y = nbaccidentparsemaine)) +
     geom_line() +
-        labs(title = "Répartition des accidents en fonction 
-             des conditions atmosphériques", x = "Numéro de semaine", y = "Nombre d'accidents" ) +
-        theme_minimal()
+        labs(title = "Série chrologique des accidents en fonction des semaines", x = "Numéro de semaine", y = "Nombre d'accidents" ) +
+        theme()
     print("On remarque que la distribution générale des 
     accidents dans le temps ne suit pas un modèle linéaire.
     Cependant, et justement car la variabilité des accidents 
@@ -188,4 +201,10 @@ affiche_serie_semaine <- function(){
     conviendrait mieux à la
     distribution mensuelle des accidents.  ")
     print(figure4)
+    if(CUMUL== 0){
+    ggsave("figure/figureseriesemaine.png", figure4, dpi =300)
+    }
+    if(CUMUL== 1){
+    ggsave("figure/figureseriesemainecumul.png", figure4, dpi =300)
+    }
 }
--- main.r
+++ main.r
@@ -1,49 +1,11 @@
 source("RdeJuju.r")
 library(ggplot2)
-BDD <- read.csv2(file = "stat_juju.csv")
-library(ggplot2)
-BDD[BDD == "NULL"] <- " NA"
-dim(BDD)
-names(BDD)
-summary(BDD)
-head(BDD)
-nombre <- dim(BDD)[1]
-print(nombre)
-counts <- unique(BDD$descr_athmo)
-BDD$descr_grav[BDD$descr_grav == "Indemne"] <- 0
-BDD$descr_grav[BDD$descr_grav == "Blessé léger"] <- 1
-BDD$descr_grav[BDD$descr_grav == "Blessé hospitalisé"] <- 2
-BDD$descr_grav[BDD$descr_grav == "Tué"] <- 3
-BDD2 <- read.csv2(file = "stat_juju3.csv", sep = ",")
-# affiche_pie_athmo()
-# affiche_pie_surface()
-# affiche_barre_ville()
-# affiche_pie_gravite()
-# affiche_pie_heure()
-# affiche_serie_mois()
-# affiche_serie_semaine()
-
-print("fin")
-library(ggplot2)
-library(maps)
-library(mapdata)
-library(ggmap)
-
-data <- data.frame(
-  region_name = c("Île-de-France", "Provence-Alpes-Côte d'Azur", "Auvergne-Rhône-Alpes"),
-  nombre_accidents = c(500, 300, 200),
-  couleur = c("blue", "green", "red")
-)
-
-# Obtenez les données géographiques pour la France
-france_map <- map_data("france")
-
-# Fusionnez les données géographiques avec les données de nombre d'accidents par région
-merged_data <- merge(france_map, data, by.x = "region", by.y = "region_name", all.x = TRUE)
-
-# Créez le graphique
-print(ggplot() +
-  geom_polygon(data = merged_data, aes(x = long, y = lat, group = group, fill = couleur)) +
-  labs(title = "Nombre d'accidents par région en France") +
-  scale_fill_manual(values = data$couleur) +
-  theme_void())
+affiche_pie_athmo()
+affiche_pie_surface()
+#affiche_barre_ville()
+affiche_pie_gravite()
+#affiche_pie_heure()
+affiche_serie_mois(0)
+affiche_serie_semaine(0)
+affiche_serie_mois(1)
+affiche_serie_semaine(1)
